027aaf3e1e022c5703332f27dc19a30cf3195b76
Kai-Schewina <57536920+Kai-Schewina@users.noreply.github.com>
2022-06-06 17:04:16 +0200

    simplified old version + added new approach

--- model/main.py
+++ model/main.py
@@ -11,7 +11,6 @@ import pandas as pd
 from build.readers import InHospitalMortalityReader
 from preprocessing import Discretizer, Normalizer
 import metrics
-import keras_utils
 import utils
 from lstm import Network
 
@@ -20,7 +19,7 @@ from keras.layers.core import Dense
 from keras.models import Model
 
 
-def main(target_repl_coef=0.0, data='../data/in-hospital-mortality/', output_dir='.', dim=256, depth=1, epochs=3,
+def main(data='../data/in-hospital-mortality/', output_dir='.', dim=256, depth=1, epochs=3,
          load_state="", mode="train", batch_size=64, l2=0, l1=0, save_every=1, prefix="", dropout=0.0, rec_dropout=0.0,
          batch_norm=False, timestep=1.0, imputation="previous", small_part=True, whole_data=False, optimizer="adam",
          lr=0.001, beta_1=0.9, verbose=2, normalizer_state="./ihm_ts_1.00_impute_previous_start_zero_masks_True_n_20918.normalizer",
@@ -28,9 +27,7 @@ def main(target_repl_coef=0.0, data='../data/in-hospital-mortality/', output_dir
     
     if small_part:
         save_every = 2 ** 30
-    
-    target_repl = (target_repl_coef > 0.0 and mode == 'train')
-    
+
     # Build readers, discretizers, normalizers
     train_reader = InHospitalMortalityReader(dataset_dir=os.path.join(data, 'train'),
                                              listfile=os.path.join(data, 'train_listfile.csv'))
@@ -52,13 +49,13 @@ def main(target_repl_coef=0.0, data='../data/in-hospital-mortality/', output_dir
     
     # Build the model
     model = Network(dim=dim, batch_norm=batch_norm, dropout=dropout, rec_dropout=rec_dropout, task='ihm',
-                    target_repl=target_repl, deep_supervision=False, num_classes=1, depth=depth,
-                    input_dim=input_dim)
-    suffix = ".bs{}{}{}.ts{}{}".format(batch_size,
-                                       ".L1{}".format(l1) if l1 > 0 else "",
-                                       ".L2{}".format(l2) if l2 > 0 else "",
-                                       timestep,
-                                       ".trc{}".format(target_repl_coef) if target_repl_coef > 0 else "")
+                    num_classes=1, depth=depth, input_dim=input_dim)
+
+    suffix = "_bs{}{}{}_ts{}".format(batch_size,
+                                     "_L1{}".format(l1) if l1 > 0 else "",
+                                     "_L2{}".format(l2) if l2 > 0 else "",
+                                     timestep)
+
     model.final_name = prefix + model.say_name() + suffix
     print("==> model.final_name:", model.final_name)
     
@@ -69,17 +66,9 @@ def main(target_repl_coef=0.0, data='../data/in-hospital-mortality/', output_dir
                                    'beta_1': beta_1}}
 
     loss = 'binary_crossentropy'
-
-    if balance_loss:
-        train_file = pd.read_csv(os.path.join(data, 'train_listfile.csv'))
-        train_trues = np.array(train_file["y_true"])
-        loss_weights = class_weight.compute_class_weight('balanced', classes=np.unique(train_trues), y=train_trues)
-    else:
-        loss_weights = None
     
     model.compile(optimizer=optimizer_config,
-                  loss=loss,
-                  loss_weights=loss_weights)
+                  loss=loss)
     model.summary()
     
     # Load model weights
@@ -92,30 +81,13 @@ def main(target_repl_coef=0.0, data='../data/in-hospital-mortality/', output_dir
     train_raw = utils.load_data(train_reader, discretizer, normalizer, small_part)
     val_raw = utils.load_data(val_reader, discretizer, normalizer, small_part)
     
-    if target_repl:
-        T = train_raw[0][0].shape[0]
-    
-        def extend_labels(data):
-            data = list(data)
-            labels = np.array(data[1])  # (B,)
-            data[1] = [labels, None]
-            data[1][1] = np.expand_dims(labels, axis=-1).repeat(T, axis=1)  # (B, T)
-            data[1][1] = np.expand_dims(data[1][1], axis=-1)  # (B, T, 1)
-            return data
-    
-        train_raw = extend_labels(train_raw)
-        val_raw = extend_labels(val_raw)
-    
     if mode == 'train':
     
         # Prepare training
         path = os.path.join(output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
     
-        metrics_callback = keras_utils.InHospitalMortalityMetrics(train_data=train_raw,
-                                                                  val_data=val_raw,
-                                                                  target_repl=(target_repl_coef > 0),
-                                                                  batch_size=batch_size,
-                                                                  verbose=verbose)
+        metrics_callback = metrics.InHospitalMortalityMetrics(train_data=train_raw, val_data=val_raw,
+                                                              batch_size=batch_size, verbose=verbose)
         # make sure save directory exists
         dirname = os.path.dirname(path)
         if not os.path.exists(dirname):
---
+++ model/main_v2.py
@@ -0,0 +1,144 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import re
+import metrics
+import pickle
+import numpy as np
+import utils
+from tensorflow.keras import layers
+from sklearn.utils.class_weight import compute_class_weight
+
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow import keras
+
+
+def build_model(depth, units, dropout, input_dim):
+    model = keras.Sequential()
+    model.add(layers.Input(shape=(None, input_dim)))
+    model.add(layers.Masking())
+    for i in range(depth - 1):
+        model.add(layers.Bidirectional(layers.LSTM(units=int(units/2), return_sequences=True,
+                                                   dropout=dropout, recurrent_dropout=dropout)))
+    model.add(layers.LSTM(units=units))
+    model.add(layers.Dropout(dropout))
+    model.add(layers.Dense(1, activation="sigmoid"))
+    return model
+
+
+def main(data='../data/in-hospital-mortality_v3/', output_dir='.', dim=256, depth=1, epochs=20,
+         load_state="", mode="train", batch_size=64, l2=0, l1=0, save_every=1, prefix="", dropout=0.0, rec_dropout=0.0,
+         batch_norm=False, timestep=1.0, small_part=False, optimizer="adam",
+         lr=0.001, beta_1=0.9, verbose=2, balance=False, balance_loss=False):
+
+    def weighted_bincrossentropy(y_true, y_pred, weight_zero=0.125, weight_one=0.875):
+        bin_crossentropy = keras.backend.binary_crossentropy(y_true, y_pred)
+
+        weights = y_true * weight_one + (1. - y_true) * weight_zero
+        weighted_bin_crossentropy = weights * bin_crossentropy
+
+        return keras.backend.mean(weighted_bin_crossentropy)
+
+    # Load Data
+    if mode == "train":
+        with open(data + "/train_raw.pkl", "rb") as f:
+            train_raw = pickle.load(f)
+            input_dim = train_raw[0].shape[2]
+        with open(data + "/val_raw.pkl", "rb") as f:
+            val_raw = pickle.load(f)
+    elif mode == "test":
+        with open(data + "/test.pkl", "rb") as f:
+            test = pickle.load(f)
+            input_dim = test["data"][0].shape[2]
+    else:
+        raise ValueError("Wrong value for mode")
+
+    # Setup
+    if small_part:
+        save_every = 2 ** 30
+
+    optimizer_config = {'class_name': optimizer,
+                        'config': {'lr': lr,
+                                   'beta_1': beta_1}}
+    loss = 'binary_crossentropy'
+
+
+    # Build the model
+    model = build_model(depth, dim, dropout, input_dim)
+
+    suffix = "_bs{}{}{}_ts{}".format(batch_size,
+                                     "_L1{}".format(l1) if l1 > 0 else "",
+                                     "_L2{}".format(l2) if l2 > 0 else "",
+                                     timestep)
+    say_name = "{}.n{}{}{}{}.dep{}".format('k_lstm',
+                                           dim,
+                                           ".bn" if batch_norm else "",
+                                           ".d{}".format(dropout) if dropout > 0 else "",
+                                           ".rd{}".format(rec_dropout) if rec_dropout > 0 else "",
+                                           depth)
+    model.final_name = prefix + say_name + suffix
+    model.compile(optimizer=optimizer_config,
+                  loss=weighted_bincrossentropy)
+    model.summary()
+
+    # Load model weights
+    n_trained_chunks = 0
+    if load_state != "":
+        model.load_weights(load_state)
+        n_trained_chunks = int(re.match(".*epoch([0-9]+).*", load_state).group(1))
+
+    if mode == 'train':
+        # Balancing
+        class_weight = None
+        if balance:
+            weight_for_0 = (1 / len(train_raw[1])) * (len(train_raw[1]) / 2.0)
+            weight_for_1 = (1 / sum(train_raw[1])) * (len(train_raw[1]) / 2.0)
+            # class_weight = {0: weight_for_0, 1: weight_for_1}
+            print('Weight for class 0: {:.2f}'.format(weight_for_0))
+            print('Weight for class 1: {:.2f}'.format(weight_for_1))
+            # class_weight = compute_class_weight("balanced", classes=np.unique(train_raw[1]), y=train_raw[1])
+
+        # Prepare training
+        path = os.path.join(output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
+        os.makedirs(path, exist_ok=True)
+        saver = ModelCheckpoint(path, verbose=1, period=save_every)
+        keras_logs = os.path.join(output_dir, 'keras_logs')
+        os.makedirs(keras_logs, exist_ok=True)
+        csv_logger = CSVLogger(os.path.join(keras_logs, model.final_name + '.csv'),
+                               append=True, separator=';')
+
+        metrics_callback = metrics.InHospitalMortalityMetrics(train_data=train_raw,
+                                                              val_data=val_raw,
+                                                              batch_size=batch_size,
+                                                              verbose=verbose)
+
+        print("==> training")
+        model.fit(x=train_raw[0],
+                  y=train_raw[1],
+                  validation_data=val_raw,
+                  epochs=n_trained_chunks + epochs,
+                  initial_epoch=n_trained_chunks,
+                  callbacks=[metrics_callback, saver, csv_logger],
+                  shuffle=True,
+                  verbose=verbose,
+                  batch_size=batch_size,
+                  class_weight=class_weight)
+
+    if mode == 'test':
+        data = test["data"][0]
+        labels = test["data"][1]
+        names = test["names"]
+
+        predictions = model.predict(data, batch_size=batch_size, verbose=1)
+        predictions = np.array(predictions)[:, 0]
+        metrics.print_metrics_binary(labels, predictions)
+
+        path = os.path.join(output_dir, "test_predictions", os.path.basename(load_state)) + ".csv"
+        utils.save_results(names, predictions, labels, path)
+
+
+if __name__ == "__main__":
+    main(mode="train", dropout=0.2, balance=False, depth=5, batch_size=256, dim=48, epochs=100, lr=0.01)
+
