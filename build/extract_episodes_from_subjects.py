cf3ca6b2c100cf283a8b1a3f75513da8f5b136df
Kai-Schewina <57536920+Kai-Schewina@users.noreply.github.com>
2022-06-07 20:38:45 +0200

    removed unnecessary comments

--- build/extract_episodes_from_subjects.py
+++ build/extract_episodes_from_subjects.py
@@ -7,11 +7,7 @@ from tqdm import tqdm
 import multiprocessing as mp
 from functools import partial
 
-# Runtime 1-2h
-# Multiprocessing speeds it up to about 20min in my case
-
-from subject import read_stays, read_diagnoses, read_events, get_events_for_stay,\
-    add_hours_elpased_to_events
+from subject import read_stays, read_events, get_events_for_stay, add_hours_elpased_to_events
 from subject import convert_events_to_timeseries, get_first_valid_from_timeseries
 from preprocessing import read_itemid_to_variable_map, map_itemids_to_variables, clean_events
 from preprocessing import assemble_episodic_data
