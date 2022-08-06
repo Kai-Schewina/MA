93f89bc3aba69bfea1340e445ceb29d69ba665d1
Kai-Schewina <57536920+Kai-Schewina@users.noreply.github.com>
2022-08-06 16:36:47 +0200

    added resampling for testing

--- model/main_v2.py
+++ model/main_v2.py
@@ -19,18 +19,15 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 from tensorflow.keras.metrics import AUC, BinaryAccuracy, Precision, Recall
 import matplotlib.pyplot as plt
+from metrics import f1
 
 
 def build_model(depth, units, dropout, rec_dropout, input_dim, kernel_reg):
     model = keras.Sequential()
     model.add(Input(shape=(None, input_dim)))
     for i in range(depth - 1):
-        model.add(Bidirectional(LSTM(units=int(units/2), return_sequences=True, dropout=dropout, recurrent_dropout=rec_dropout, kernel_regularizer=L2(kernel_reg),
-                       recurrent_regularizer=L2(0.00), bias_regularizer=L2(0.00),
-                       activity_regularizer=L2(0.0))))
-    model.add(LSTM(units=units, dropout=dropout, recurrent_dropout=rec_dropout, kernel_regularizer=L2(kernel_reg),
-                   recurrent_regularizer=L2(0.00), bias_regularizer=L2(0.00),
-                   activity_regularizer=L2(0.0)))
+        model.add(Bidirectional(LSTM(units=int(units/2), return_sequences=True, dropout=dropout, recurrent_dropout=rec_dropout)))
+    model.add(LSTM(units=units, dropout=dropout, recurrent_dropout=rec_dropout))
     model.add(Dropout(rate=dropout))
     model.add(Dense(1, activation="sigmoid"))
     return model
@@ -38,16 +35,8 @@ def build_model(depth, units, dropout, rec_dropout, input_dim, kernel_reg):
 
 def main(data, output_dir='.', dim=256, depth=1, epochs=20,
          load_state="", mode="train", batch_size=64, l2=0, l1=0, save_every=1, prefix="", dropout=0.0, rec_dropout=0.0,
-         batch_norm=False, timestep=1.0, small_part=False, optimizer="adam", kernel_reg=0.0,
-         lr=0.001, beta_1=0.9, verbose=2, balance=False, balance_loss=False, balanced_sample=False):
-
-    def weighted_bincrossentropy(y_true, y_pred, weight_zero=0.28, weight_one=0.72):
-        bin_crossentropy = keras.backend.binary_crossentropy(y_true, y_pred)
-
-        weights = y_true * weight_one + (1. - y_true) * weight_zero
-        weighted_bin_crossentropy = weights * bin_crossentropy
-
-        return keras.backend.mean(weighted_bin_crossentropy)
+         batch_norm=False, timestep=1.0, small_part=False, kernel_reg=0.0,
+         lr=0.001, verbose=2, balance=False, balance_loss=False, balanced_sample=False):
 
     # Load Data
     if mode == "train":
@@ -64,7 +53,7 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         with open(data + "/val_raw.pkl", "rb") as f:
             val_raw = pickle.load(f)
             val_raw = (np.asarray(val_raw["data"][0]).astype('float32'), np.asarray(val_raw["data"][1]).astype('float32'))
-    elif mode == "test" or mode == "cv_test":
+    elif mode == "test" or mode == "bootstrap_test":
         with open(data + "/test.pkl", "rb") as f:
             test = pickle.load(f)
             input_dim = test["data"][0].shape[2]
@@ -79,10 +68,7 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
     if small_part:
         save_every = 2 ** 30
 
-    if balance_loss:
-        loss = weighted_bincrossentropy
-    else:
-        loss = 'binary_crossentropy'
+    loss = 'binary_crossentropy'
 
     # Build the model
     model = build_model(depth, dim, dropout, rec_dropout, input_dim, kernel_reg)
@@ -99,9 +85,12 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
                                            ".lr{}".format(lr) if lr > 0 else "",
                                            depth)
     model.final_name = prefix + say_name + suffix
-    model.compile(optimizer=Adam(learning_rate=lr),
+
+    opt = Adam(learning_rate=lr)
+    my_metrics = [AUC(), AUC(curve="PR"), BinaryAccuracy(), Precision(), Recall()]
+    model.compile(optimizer=opt,
                   loss=loss,
-                  metrics=[AUC(), AUC(curve="PR"), BinaryAccuracy(), Precision(), Recall(), metrics.f1])
+                  metrics=my_metrics)
     model.summary()
 
     # Load model weights
@@ -111,15 +100,9 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         n_trained_chunks = int(re.match(".*epoch([0-9]+).*", load_state).group(1))
 
     if mode == 'train':
-        # Balancing
-        class_weight = None
-        if balance:
-            weight_for_0 = (1 / len(train_raw[1])) * (len(train_raw[1]) / 2.0)
-            weight_for_1 = (1 / sum(train_raw[1])) * (len(train_raw[1]) / 2.0)
-            class_weight = {0: weight_for_0, 1: weight_for_1}
-            print('Weight for class 0: {:.2f}'.format(weight_for_0))
-            print('Weight for class 1: {:.2f}'.format(weight_for_1))
-
+        print("Initial Performance:")
+        eval = model.evaluate(val_raw[0], val_raw[1], batch_size=batch_size)
+        print(eval)
         # Prepare training
         path = os.path.join(output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
         os.makedirs(path, exist_ok=True)
@@ -139,14 +122,12 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         history = model.fit(x=train_raw[0],
                   y=train_raw[1],
                   validation_data=(val_raw[0], val_raw[1]),
-                  # validation_split=0.2,
                   epochs=n_trained_chunks + epochs,
                   initial_epoch=n_trained_chunks,
-                  callbacks=[metrics_callback, saver, csv_logger, early_stopping],
+                  callbacks=[metrics_callback, saver, csv_logger],
                   shuffle=True,
                   verbose=verbose,
-                  batch_size=batch_size,
-                  class_weight=class_weight)
+                  batch_size=batch_size)
         # Visualize history
         # Plot history: Loss
         plt.plot(history.history['val_loss'])
@@ -164,7 +145,7 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         plt.xlabel('No. epoch')
         plt.show()
 
-    if mode == 'test':
+    elif mode == 'test':
         data = np.asarray(test["data"][0]).astype('float32')
         labels = np.asarray(test["data"][1]).astype('float32')
         names = test["names"]
@@ -179,7 +160,7 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         eval = model.evaluate(data, labels, batch_size=batch_size)
         print(eval)
 
-    if mode == 'val_test':
+    elif mode == 'val_test':
         data = np.asarray(test[0]).astype('float32')
         labels = np.asarray(test[1]).astype('float32')
 
@@ -189,28 +170,28 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         eval = model.evaluate(data, labels, batch_size=batch_size)
         print(eval)
 
-    if mode == "cv_test":
+    elif mode == "bootstrap_test":
         data = np.asarray(test["data"][0]).astype('float32')
         labels = np.asarray(test["data"][1]).astype('float32')
 
         # Execute Random Sampling
-        for i in tqdm(range(1000)):
+        for i in tqdm(range(10000)):
             data_sample, labels_sample = resample(data, labels, replace=True, random_state=np.random.RandomState(42+i))
 
             eval = model.evaluate(data_sample, labels_sample, batch_size=batch_size, verbose=0)
             if i == 0:
                 eval_df = pd.DataFrame([eval])
-                eval_df.columns = ["loss", "auroc", "auprc", "accuracy", "precision", "recall", "f1"]
+                eval_df.columns = ["loss", "auroc", "auprc", "accuracy", "precision", "recall"]
             else:
                 eval_df.loc[i] = eval
 
+        eval_df["f1"] = 2 * (eval_df["precision"] * eval_df["recall"]) / (eval_df["precision"] + eval_df["recall"])
         os.makedirs("./cv_test/", exist_ok=True)
         eval_df.to_csv("./cv_test/" + model.final_name + ".csv", index=False)
 
 
-
 if __name__ == "__main__":
     path = "../data/in-hospital-mortality_v7/"
-    main(data=path, mode="train", dropout=0.3, rec_dropout=0.0, depth=2, batch_size=8, dim=16, epochs=30,
-         balanced_sample=False, balance=False, kernel_reg=0.00, lr=0.001,
-         load_state="")
+    main(data=path, mode="bootstrap_test", dropout=0.4, rec_dropout=0.3, depth=3, batch_size=32, dim=64, epochs=4,
+         balanced_sample=True, balance=False, lr=0.01,
+         load_state="./keras_states/k_lstm.n64.d0.4.rd0.3.dep.lr0.01_bs32_ts1.0.epoch4.test0.2864157259464264.state")
