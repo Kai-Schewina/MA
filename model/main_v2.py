f0024ad692836ff5585dcd030121c05e785ec1d1
Kai-Schewina <57536920+Kai-Schewina@users.noreply.github.com>
2022-08-12 15:06:33 +0200

    added hyperparameter tuning for transfer

--- model/main_v2.py
+++ model/main_v2.py
@@ -2,7 +2,22 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import os
+import random
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['PYTHONHASHSEED'] = '42'
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
+os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+from numpy.random import seed
+seed(42)
+random.seed(42)
+import tensorflow as tf
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(1)
+tf.random.set_seed(42)
+
 import re
 import metrics
 import pickle
@@ -19,7 +34,6 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 from tensorflow.keras.metrics import AUC, BinaryAccuracy, Precision, Recall
 import matplotlib.pyplot as plt
-from metrics import f1
 
 
 def build_model(depth, units, dropout, rec_dropout, input_dim, kernel_reg):
@@ -28,7 +42,7 @@ def build_model(depth, units, dropout, rec_dropout, input_dim, kernel_reg):
     for i in range(depth - 1):
         model.add(Bidirectional(LSTM(units=int(units/2), return_sequences=True, dropout=dropout, recurrent_dropout=rec_dropout)))
     model.add(LSTM(units=units, dropout=dropout, recurrent_dropout=rec_dropout))
-    model.add(Dropout(rate=dropout))
+    model.add(Dropout(rate=dropout, seed=42))
     model.add(Dense(1, activation="sigmoid"))
     return model
 
@@ -36,7 +50,7 @@ def build_model(depth, units, dropout, rec_dropout, input_dim, kernel_reg):
 def main(data, output_dir='.', dim=256, depth=1, epochs=20,
          load_state="", mode="train", batch_size=64, l2=0, l1=0, save_every=1, prefix="", dropout=0.0, rec_dropout=0.0,
          batch_norm=False, timestep=1.0, small_part=False, kernel_reg=0.0,
-         lr=0.001, verbose=2, balance=False, balance_loss=False, balanced_sample=False):
+         lr=0.001, verbose=2, balance=False, balance_loss=False, balanced_sample=False, percentage=None):
 
     # Load Data
     if mode == "train":
@@ -57,13 +71,14 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         with open(data + "/test.pkl", "rb") as f:
             test = pickle.load(f)
             input_dim = test["data"][0].shape[2]
-    elif mode == "val_test":
-        with open(data + "/val_raw.pkl", "rb") as f:
-            test = pickle.load(f)
-            input_dim = test[0].shape[2]
     else:
         raise ValueError("Wrong value for mode")
 
+    # Potential Subsampling
+    if percentage is not None:
+        np.random.seed(42)
+        idx = np.random.choice(np.arange(len(train_raw[0])), int(len(train_raw[0]) * percentage), replace=False)
+        train_raw = (train_raw[0][idx], train_raw[1][idx])
     # Setup
     if small_part:
         save_every = 2 ** 30
@@ -104,9 +119,15 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         eval = model.evaluate(val_raw[0], val_raw[1], batch_size=batch_size)
         print(eval)
         # Prepare training
-        path = os.path.join(output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
+        if percentage is not None:
+            path = os.path.join(output_dir, 'keras_states/' + model.final_name + "_perc_" + str(percentage))
+            saver = ModelCheckpoint(path, verbose=verbose, period=save_every, save_best_only=True)
+        else:
+            path = os.path.join(output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
+            saver = ModelCheckpoint(path, verbose=verbose, period=save_every)
         os.makedirs(path, exist_ok=True)
-        saver = ModelCheckpoint(path, verbose=1, period=save_every)
+
+
         keras_logs = os.path.join(output_dir, 'keras_logs')
         os.makedirs(keras_logs, exist_ok=True)
         csv_logger = CSVLogger(os.path.join(keras_logs, model.final_name + '.csv'),
@@ -116,8 +137,6 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
                                                               val_data=val_raw,
                                                               batch_size=batch_size,
                                                               verbose=verbose)
-        early_stopping = EarlyStopping(patience=5)
-
         print("==> training")
         history = model.fit(x=train_raw[0],
                   y=train_raw[1],
@@ -137,36 +156,23 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         plt.xlabel('No. epoch')
         plt.show()
 
-        # Plot history: Accuracy
-        plt.plot(history.history['val_auc_1'])
-        plt.plot(history.history['auc_1'])
-        plt.title('Validation auprc history')
-        plt.ylabel('auprc value')
-        plt.xlabel('No. epoch')
-        plt.show()
+        if percentage is not None:
+            return path
+        else:
+            return path.replace("{epoch}", str(epochs)).replace("{val_loss}", str(history.history['val_loss'][epochs-1]))
 
     elif mode == 'test':
         data = np.asarray(test["data"][0]).astype('float32')
         labels = np.asarray(test["data"][1]).astype('float32')
         names = test["names"]
 
-        predictions = model.predict(data, batch_size=batch_size, verbose=1)
+        predictions = model.predict(data, batch_size=batch_size, verbose=verbose)
         predictions = np.array(predictions)[:, 0]
         metrics.print_metrics_binary(labels, predictions)
 
         path = os.path.join(output_dir, "test_predictions", os.path.basename(load_state)) + ".csv"
         utils.save_results(names, predictions, labels, path)
 
-        eval = model.evaluate(data, labels, batch_size=batch_size)
-        print(eval)
-
-    elif mode == 'val_test':
-        data = np.asarray(test[0]).astype('float32')
-        labels = np.asarray(test[1]).astype('float32')
-
-        predictions = model.predict(data, batch_size=batch_size, verbose=1)
-        predictions = np.array(predictions)[:, 0]
-        metrics.print_metrics_binary(labels, predictions)
         eval = model.evaluate(data, labels, batch_size=batch_size)
         print(eval)
 
@@ -178,20 +184,19 @@ def main(data, output_dir='.', dim=256, depth=1, epochs=20,
         for i in tqdm(range(10000)):
             data_sample, labels_sample = resample(data, labels, replace=True, random_state=np.random.RandomState(42+i))
 
-            eval = model.evaluate(data_sample, labels_sample, batch_size=batch_size, verbose=0)
+            eval = model.evaluate(data_sample, labels_sample, batch_size=batch_size, verbose=verbose)
             if i == 0:
                 eval_df = pd.DataFrame([eval])
                 eval_df.columns = ["loss", "auroc", "auprc", "accuracy", "precision", "recall"]
             else:
                 eval_df.loc[i] = eval
-
         eval_df["f1"] = 2 * (eval_df["precision"] * eval_df["recall"]) / (eval_df["precision"] + eval_df["recall"])
         os.makedirs("./cv_test/", exist_ok=True)
-        eval_df.to_csv("./cv_test/" + model.final_name + ".csv", index=False)
+        eval_df.to_csv("./cv_test/" + model.final_name + "balanced_sample_" + str(balanced_sample) + ".csv", index=False)
 
 
 if __name__ == "__main__":
     path = "../data/in-hospital-mortality_v7/"
-    main(data=path, mode="bootstrap_test", dropout=0.4, rec_dropout=0.3, depth=3, batch_size=32, dim=64, epochs=4,
-         balanced_sample=True, balance=False, lr=0.01,
-         load_state="./keras_states/k_lstm.n64.d0.4.rd0.3.dep.lr0.01_bs32_ts1.0.epoch4.test0.2864157259464264.state")
+    main(data=path, mode="bootstrap_test", dropout=0.0, rec_dropout=0.2, depth=1, batch_size=32, dim=96, epochs=2,
+         balanced_sample=False, lr=0.001, percentage=None, verbose=0,
+         load_state="")
